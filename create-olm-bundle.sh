57aaa5cefa296dcb603f8e1c8b71b0a90af96d23
andreaTP <andrea.peruffo1982@gmail.com>
2022-03-17 16:54:44 +0000

    try it out

--- create-olm-bundle.sh
+++ create-olm-bundle.sh
@@ -2,9 +2,11 @@
 
 VERSION=$1
 REPLACES_VERSION=$2
+CREATED_AT=$(date "+%D %T")
 
 echo "Creating OLM bundle for version $VERSION replacing version $REPLACES_VERSION"
 
+rm -rf $VERSION/olm
 mkdir -p $VERSION/olm
 
 cp -r olm-base/* $VERSION/olm
@@ -13,13 +15,12 @@ cp -r olm-base/* $VERSION/olm
 yq ea '.rules as $item ireduce ({}; .rules += $item )' $VERSION/kubernetes/kubernetes.yml | \
   yq ea -i 'select(fileIndex==0).spec.install.spec.permissions = select(fileIndex==1) | select(fileIndex==0)' $VERSION/olm/manifests/clusterserviceversion.yaml -
 
-# Populate placeholder values
-export REPLACE_ME_VERSION=$VERSION
-export REPLACE_ME_LAST_VERSION=$REPLACES_VERSION
-export REPLACE_ME_CREATED_AT=$(date "+%D %T")
+yq ea -i ".metadata.annotations.containerImage = \"quay.io/keycloak/keycloak-operator:$VERSION\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
+yq ea -i ".metadata.annotations.createdAt = \"$CREATED_AT\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
+yq ea -i ".metadata.name = \"keycloak-operator.v$VERSION\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
+yq ea -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].image = \"quay.io/keycloak/keycloak-operator:$VERSION\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
+yq ea -i ".spec.replaces = \"$REPLACES_VERSION\"" $VERSION/olm/manifests/clusterserviceversion.yaml
 
-envsubst < "$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml" > "$VERSION/olm/manifests/clusterserviceversion.yaml"
-rm $VERSION/olm/manifests/clusterserviceversion.yaml
+mv $VERSION/olm/manifests/clusterserviceversion.yaml "$VERSION/olm/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml"
 
-# TODO: check if this actually changes the file
-[ "$VERSION" = "latest" ] && echo "skip storing version" || echo "$REPLACES_VERSION" > latest_version.txt
+[ "$VERSION" = "latest" ] && echo "skip storing version" || echo "$VERSION" > latest_version.txt
