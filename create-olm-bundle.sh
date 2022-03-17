6ee9c1fd37b6e7c655ca5755599370eba0990258
andreaTP <andrea.peruffo1982@gmail.com>
2022-03-17 16:20:10 +0000

    fixes?

--- create-olm-bundle.sh
+++ create-olm-bundle.sh
@@ -11,14 +11,14 @@ cp -r olm-base/* $VERSION/olm
 
 # Inject RBAC rules
 yq ea '.rules as $item ireduce ({}; .rules += $item )' $VERSION/kubernetes/kubernetes.yml | \
-  yq ea 'select(fileIndex==0).spec.install.spec.permissions = select(fileIndex==1) | select(fileIndex==0)' $VERSION/olm/manifests/clusterserviceversion.yaml
+  yq ea -i 'select(fileIndex==0).spec.install.spec.permissions = select(fileIndex==1) | select(fileIndex==0)' $VERSION/olm/manifests/clusterserviceversion.yaml -
 
 # Populate placeholder values
 export REPLACE_ME_VERSION=$VERSION
 export REPLACE_ME_LAST_VERSION=$REPLACES_VERSION
 export REPLACE_ME_CREATED_AT=$(date "+%D %T")
 
-envsubst < "$VERSION/olm/manifests/clusterserviceversion.yaml" > "$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml"
+envsubst < "$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml" > "$VERSION/olm/manifests/clusterserviceversion.yaml"
 rm $VERSION/olm/manifests/clusterserviceversion.yaml
 
 # TODO: check if this actually changes the file
