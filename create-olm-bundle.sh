cabfd31ebdf64aae676a3623daecd9ea93c4fd80
andreaTP <andrea.peruffo1982@gmail.com>
2022-03-23 11:35:09 +0000

    more fixes

--- create-olm-bundle.sh
+++ create-olm-bundle.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -x
 
 VERSION=$1
 REPLACES_VERSION=$2
@@ -12,11 +13,9 @@ mkdir -p $VERSION/olm
 cp -r olm-base/* $VERSION/olm
 
 # Inject RBAC rules
-#  | .rules[-1] += "serviceAccountName = keycloak-operator" ??
-# TODO: verify better: https://github.com/k8s-operatorhub/community-operators/pull/757/files#diff-5145e4855a6359acb4f5eb9c81e68b56c5aaad6710e986a2dd80a485a15b7d2aR332
-yq ea '.rules as $item ireduce ({}; .rules += $item) | .rules[-1] += "serviceAccountName = keycloak-operator" | .' $VERSION/kubernetes/kubernetes.yml | \
-  yq ea -i 'select(fileIndex==0).spec.install.spec.permissions[0] = select(fileIndex==1) | select(fileIndex==0)' $VERSION/olm/manifests/clusterserviceversion.yaml -
-
+yq ea '.rules as $item ireduce ({}; .rules += $item)' $VERSION/kubernetes/kubernetes.yml | \
+  yq ea -i 'select(fileIndex==0).spec.install.spec.permissions[0] = select(fileIndex==1) | select(fileIndex==0)' $VERSION/olm/manifests/clusterserviceversion.yaml - && \
+yq ea -i '.spec.install.spec.permissions[0].serviceAccountName = "keycloak-operator"' $VERSION/olm/manifests/clusterserviceversion.yaml && \
 yq ea -i ".metadata.annotations.containerImage = \"quay.io/keycloak/keycloak-operator:$VERSION\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
 yq ea -i ".metadata.annotations.createdAt = \"$CREATED_AT\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
 yq ea -i ".metadata.name = \"keycloak-operator.v$VERSION\"" $VERSION/olm/manifests/clusterserviceversion.yaml && \
